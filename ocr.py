3bd2ff76056b8d63d2fbfe2dcdbc21578028cf07
Amruta <pamruta@live.com>
2017-06-12 11:15:29 +0530

    Optical Character Recognition

---
+++ ocr.py
@@ -0,0 +1,29 @@
+# implements optical character recognition (OCR)
+# to extract text from images 
+# using Google's Cloud Vision API
+
+# Usage: ocr.py IMAGE-FILE
+
+# importing libraries
+import io
+import sys
+import re
+
+# importing google cloud-vision library
+from google.cloud import vision
+
+vision_client = vision.Client()
+
+# read image file
+with io.open(sys.argv[1], 'rb') as image_file:
+	content = image_file.read()
+
+image = vision_client.image(content = content)
+
+texts = image.detect_text()
+
+for text in texts:
+	label = text.description
+	break
+
+print label
